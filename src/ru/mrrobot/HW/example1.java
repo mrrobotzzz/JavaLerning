c26c7c8cf050d37e1a25fde1fbe2726aefabfa4f
mrrobotzzz <JoraMestniy@yandex.ru>
2017-12-01 05:04:09 -0600

    1

---
+++ src/ru/mrrobot/HW/example1.java
@@ -0,0 +1,31 @@
+package ru.mrrobot.HW;
+
+import java.io.FileNotFoundException;
+
+public class example1 {
+    public static void main(String[] args) {
+        short a = 10000;
+        byte b = (byte) a;
+        System.out.println(b);
+
+//       int size = 27;
+//       String name = "Фидо";
+//       Dog myDog = new Dog (name, size);
+//       x = size - 5;
+//       if (x <  15) myDog.bark (8);
+//       while (x > 3) {
+//           myDog.play();
+//       }
+//       int[] numList = {2,4,6,8};
+//       System.out.print("Привет");
+//       System.out.print("Собака:" + name);
+//       String num = "8";
+//       int z = Integer.parseInt(num);
+//       try {
+//           readTheFile ("myFile.txt");
+//       }
+//       catch (FileNotFoundException ex) {
+//           System.out.print("Файл не найден.");
+//       }
+    }
+}
